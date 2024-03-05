64a56621008cfa8243ee050bd234ad28738e6f21
balbasty <yael.balbastre@gmail.com>
2024-03-05 17:18:35 +0000

    MNT: fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
 
 setup(
+    packages=find_packages(),
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 )
