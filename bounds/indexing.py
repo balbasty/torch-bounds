FIX(indexing): dst1 sign

bounds/indexing.py
@@ -51,6 +51,7 @@ __all__ = [
 import torch
 from torch import Tensor
 from typing import Tuple
+from .utils import floor_div_int
 
 
 def nocheck(i, n):
@@ -259,7 +260,7 @@ def dst1_script(i, n: int) -> Tuple[Tensor, Tensor]:
     ii = torch.where(i < 0, 2*n - i, i).remainder(n2).remainder(n + 1)
     x = (ii != n).to(torch.int8)
     #   +/- ones
-    x = torch.where((i / (n + 1)).remainder(2) >= 1, -x, x)
+    x = torch.where(floor_div_int(ii, n + 1).remainder(2) >= 1, -x, x)
 
     # index
     i = torch.where(i < 0, -2 - i, i)
@@ -301,7 +302,7 @@ def dst2_int(i: int, n: int) -> Tuple[int, int]:
 @torch.jit.script
 def dst2_script(i, n: int) -> Tuple[Tensor, Tensor]:
     x = torch.ones([1], dtype=torch.int8, device=i.device)
-    x = torch.where((i / n).remainder(2) >= 1, -x, x)
+    x = torch.where(floor_div_int(i, n).remainder(2) >= 1, -x, x)
     return dct2_script(i, n)[0], x
 
 

bounds/utils.py
@@ -109,12 +109,15 @@ def torch_version(mode, version):
     return _compare_versions(current_version, mode, version)
 
 
-# floor_divide returns wrong results for negative values, because it truncates
-# instead of performing a proper floor. In recent version of pytorch, it is
-# advised to use div(..., rounding_mode='trunc'|'floor') instead.
-# Here, we only use floor_divide on positive values so we do not care.
-if torch_version('>=', (1, 8)):
-
+# In torch < 1.6, div applied to integer tensor performed a floor_divide
+# In torch > 1.6, it performs a true divide.
+# Floor division must be done using `floor_divide`, but it was buggy
+# until torch 1.13 (it was doing a trunc divide instead of a floor divide).
+# There was at some point a deprecation warning for floor_divide, but it
+# seems to have been lifted afterwards. In torch >= 1.13, floor_divide
+# performs a correct floor division.
+# Since we only apply floor_divide ot positive values, we are fine.
+if torch_version('>=', [1, 8]):
     @torch.jit.script
     def floor_div(x, y) -> torch.Tensor:
         return torch.div(x, y, rounding_mode='floor')
@@ -122,34 +125,16 @@ if torch_version('>=', (1, 8)):
     @torch.jit.script
     def floor_div_int(x, y: int) -> torch.Tensor:
         return torch.div(x, y, rounding_mode='floor')
-
-    @torch.jit.script
-    def trunc_div(x, y) -> torch.Tensor:
-        return torch.div(x, y, rounding_mode='trunc')
-
-    @torch.jit.script
-    def trunc_div_int(x, y: int) -> torch.Tensor:
-        return torch.div(x, y, rounding_mode='trunc')
-
+elif torch_version('<', (1, 6)):
+    floor_div = floor_div_int = torch.div
 else:
-
-    @torch.jit.script
-    def floor_div(x, y) -> torch.Tensor:
-        return (x / y).floor().to(x.dtype)
-
-    @torch.jit.script
-    def floor_div_int(x, y: int) -> torch.Tensor:
-        return (x / y).floor().to(x.dtype)
-
-    @torch.jit.script
-    def trunc_div(x, y) -> torch.Tensor:
-        int_dtype = torch.long if x.is_floating_point() else x.dtype
-        return (x / y).to(int_dtype).to(x.dtype)
-
-    @torch.jit.script
-    def trunc_div_int(x, y: int) -> torch.Tensor:
-        int_dtype = torch.long if x.is_floating_point() else x.dtype
-        return (x / y).to(int_dtype).to(x.dtype)
+    floor_div = floor_div_int = torch.floor_divide
+#     @torch.jit.script
+#     def floor_div(x, y) -> torch.Tensor:
+#         return (x / y).floor_()
+#     @torch.jit.script
+#     def floor_div_int(x, y: int) -> torch.Tensor:
+#         return (x / y).floor_()
 
 
 if torch_version('>=', (1, 10)):